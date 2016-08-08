df8323e9f8ddcfd6865e09710c0e2a3d05a74d14
uday krishna g <udaykrishna@gmail.com>
2016-08-09 01:16:14 +0530

    added license

--- src/Log.java
+++ src/Log.java
@@ -1,3 +1,25 @@
+// MIT License
+//
+// Copyright (c) 2016 Uday Krishna
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -37,7 +59,7 @@ private ArrayList<String> findFiles(){
 private  void moveFiles(ArrayList<String> filenames){
 	String movedir = this.movedir;
 	for(String filename:filenames){
-		File moveable = new File(path+"\\"+filename); 
+		File moveable = new File(path+"\\"+filename);
 		File dir = new File(path+"\\"+movedir);
 		if(!dir.exists()){
 			Boolean creation = dir.mkdir();
@@ -50,11 +72,11 @@ private  void moveFiles(ArrayList<String> filenames){
 		System.out.println("Moved File !");
 			}
 		}
-		
+
 public void mergeFiles(){
 	ArrayList<String> filenames=findFiles();
 	for(String filename:filenames){
-		
+
 	}
 	}
 public void createFiles(){
